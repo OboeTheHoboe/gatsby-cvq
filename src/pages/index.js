7a79144f4d94cf8b0dceef506556cfc5b30d9b59
OboeTheHoboe <jorgegponce@yahoo.com>
2021-06-11 23:20:32 -0500

    ultimo de hoy

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,36 @@ import Layout from '../components/layout'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import { graphql } from 'gatsby'
 
+const serializers = {
+  types: {
+    code: (props) => (
+      <pre data-language={props.node.language}>
+        <code>{props.node.code}</code>
+      </pre>
+    ),
+  },
+}
+
+
 const IndexPage = ({data}) => {
-  const image = getImage(data.sanityCommittee.image.asset.gatsbyImageData)
+  const integrantes = data.allSanityCommittee.nodes
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <GatsbyImage
-        image = {image} alt={data.sanityCommittee.name}
-      />
-      <h1>{data.sanityCommittee.name}</h1>
+      <p>Experimento</p>
+      <ul>
+      {integrantes.map((integrante) =>{
+        return(
+          <li>
+          <GatsbyImage
+            image = {integrante.image.asset.gatsbyImageData}
+          />
+          <h1>{integrante.name}</h1>
+          <p>{integrante.profession}</p>
+          <p>{integrante.role}</p>
+          <p>{integrante.bio[0].children[0].text}</p>
+          </ li>
+      )})}
+      </ul>
     </Layout>
   )
 }
@@ -20,18 +41,21 @@ export default IndexPage
 
 export const query = graphql`
 query {
-  sanityCommittee {
-    name
-    role
-    profession
-    accolades
-    image {
-      asset {
-        gatsbyImageData(fit: FILLMAX, placeholder: BLURRED)
+  allSanityCommittee {
+    nodes {
+      name
+      image {
+        asset {
+          gatsbyImageData
+        }
       }
-    }
-    bio {
-      _rawChildren(resolveReferences: {maxDepth: 10})
+      profession
+      role
+      bio {
+        children {
+          text
+        }
+      }
     }
   }
 }`
