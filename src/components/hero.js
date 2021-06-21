43ab79a00fb1bf00d5ae1c7599d66e1061cba06e
OboeTheHoboe <jorgegponce@yahoo.com>
2021-06-21 14:50:56 -0500

    I hate cssx2

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Link, useStaticQuery, graphql } from 'gatsby'
-import Button from './Button';
+import Button from './button';
 import './hero.css';
 
 const Hero = () =>{
